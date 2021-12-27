58b8386b157e209254cf72f107a17dd43c82f1d8
Tamiris Shigaki <shigakiendo@gmail.com>
2021-12-27 19:47:17 -0300

    Requisito 2, 3 e 4: ok

--- script.js
+++ script.js
@@ -1,9 +1,35 @@
 window.onload = function () {
-  let input = document.querySelector('#text-input');
-  input.addEventListener('keyup', insereTexto);
-  let texto = document.querySelector('#meme-text');
+  //! Requisito 1
+
+  let inputText = document.querySelector('#text-input');
+  inputText.addEventListener('keyup', insereTexto);
 
   function insereTexto() {
-    texto.innerHTML = input.value;
+    let texto = document.querySelector('#meme-text');
+    texto.innerHTML = inputText.value;
+  }
+
+  //!Requisito 2
+
+  let inputImagem = document.querySelector('#meme-insert');
+  inputImagem.addEventListener('change', selecionarImagem);
+
+  function selecionarImagem(parametro) {
+    let criaImg = document.createElement('img');
+    criaImg.id = 'meme-image';
+    let container = document.querySelector('#meme-image-container');
+
+    //todos os arquivos selecionados
+    let objeto = new FileReader();
+    let referencia = parametro.target;
+    // transforma de leitura para poder mexer
+    objeto.onload = function () {
+      //endereço da imagem
+      let resultado = objeto.result;
+      criaImg.src = resultado;
+    };
+    objeto.readAsDataURL(referencia.files[0]);
+
+    container.appendChild(criaImg);
   }
 };
