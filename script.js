01e6cf7a03117bc3bd9c9b9bf91c2902963de659
Tamiris Shigaki <shigakiendo@gmail.com>
2021-12-28 00:56:37 -0300

    Requisitos 7: ok

--- script.js
+++ script.js
@@ -10,31 +10,27 @@ window.onload = function () {
   }
 
   //!Requisito 2
+  // new FileReader() - pega todos os arquivos 'selecionados' <input type="file"> (arquivos de leitura)
+  //  x.onload - transforma os arquivos - da pra trabalhar com eles
+  // x.result - é o arquivo (caminho) em si, selecionado no input
+  // x.readAsDataURL(x.files[0]) - tras o caminho do arquivo em si
 
   let inputImagem = document.querySelector('#meme-insert');
   inputImagem.addEventListener('change', selecionarImagem);
 
   function selecionarImagem(parametro) {
-    let criaImg = document.createElement('img');
-    criaImg.id = 'meme-image';
-    let container = document.querySelector('#meme-image-container');
-
-    //todos os arquivos selecionados
+    let criaImg = document.querySelector('#meme-image');
     let objeto = new FileReader();
     let referencia = parametro.target;
-    // transforma de leitura para poder mexer
     objeto.onload = function () {
-      //endereço da imagem
       let resultado = objeto.result;
       criaImg.src = resultado;
     };
     objeto.readAsDataURL(referencia.files[0]);
-
-    container.appendChild(criaImg);
   }
 
   //! Requisito 6
-  const container = document.querySelector('#meme-image-container');
+  let container = document.querySelector('#meme-image-container');
 
   let botaoFire = document.querySelector('#fire');
   botaoFire.addEventListener('click', bordarFire);
@@ -53,4 +49,30 @@ window.onload = function () {
   function bordaEarth() {
     container.style.border = '6px groove green';
   }
+
+  //! Requisito 7
+
+  let meme1 = document.querySelector('#meme-1');
+  meme1.addEventListener('click', function () {
+    let imagem = document.querySelector('#meme-image');
+    imagem.src = './imgs/meme1.png';
+  });
+
+  let meme2 = document.querySelector('#meme-2');
+  meme2.addEventListener('click', function () {
+    let imagem = document.querySelector('#meme-image');
+    imagem.src = './imgs/meme2.png';
+  });
+
+  let meme3 = document.querySelector('#meme-3');
+  meme3.addEventListener('click', function () {
+    let imagem = document.querySelector('#meme-image');
+    imagem.src = './imgs/meme3.png';
+  });
+
+  let meme4 = document.querySelector('#meme-4');
+  meme4.addEventListener('click', function () {
+    let imagem = document.querySelector('#meme-image');
+    imagem.src = './imgs/meme4.png';
+  });
 };
